37b33cd62993ddfbaed9a08e80f73549b7ce9674
Bruno Signorelli Domingues <brunosd1@al.insper.edu.br>
2020-06-01 21:51:47 -0300

    Remove Wi-Fi info

--- Labs/RTOS-WIFI/WIFI-RTOS-get/src/main.h
+++ Labs/RTOS-WIFI/WIFI-RTOS-get/src/main.h
@@ -48,13 +48,13 @@ extern "C" {
 #include "driver/include/m2m_wifi.h"
 
 /** Wi-Fi Settings */
-#define MAIN_WLAN_SSID					"Poderosos" /**< Destination SSID */
-#define MAIN_WLAN_PSK					"05122223" /**< Password for Destination SSID */
+#define MAIN_WLAN_SSID					"" /**< Destination SSID */
+#define MAIN_WLAN_PSK					"" /**< Password for Destination SSID */
 #define MAIN_WLAN_AUTH					M2M_WIFI_SEC_WPA_PSK /**< Security manner */
 
 /** Using broadcast address for simplicity. */
 #define MAIN_SERVER_PORT				(5000)
-#define MAIN_SERVER_NAME				"192.168.100.103"
+#define MAIN_SERVER_NAME				""
 
 /** IP address parsing. */
 #define IPV4_BYTE(val, index)               ((val >> (index * 8)) & 0xFF)
